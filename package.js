360064b523d84e3211d3e878d8176af39e9b97fe
filipe_nevola <filipe.nevola@tecsinapse.com.br>
2017-12-22 13:46:11 -0300

    fixing minifier-css version

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Package.describe({
 Package.registerBuildPlugin({
   name: "minifyStdCSS",
   use: [
-    'minifier-css@1.3.5'
+    'minifier-css@1.2.16'
   ],
   npmDependencies: {
     "source-map": "0.5.6",
